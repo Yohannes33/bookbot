710b82bd8e3a5d6ae95d79db81df228d9cca209f
Yohannes33 <yohannesnegash33@gmail.com>
2026-04-23 13:26:51 +0300

    Refactor number_of_words function to improve code structure

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ def get_book_text(filepath):
     with open(filepath, "r") as f:
         return f.read()
 
+def number_of_words(text):
+    return f"Found {len(text.split())} total words"
 
 def main():
     text = get_book_text("books/frankenstein.txt")
@@ -10,5 +12,3 @@ def main():
 
 main()
 
-def number_of_words(text):
-    return f"Found {len(text.split())} total words"
