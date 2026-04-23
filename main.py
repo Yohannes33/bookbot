2d4a7c902a639c2716b8d874ae1242163964894b
Yohannes33 <yohannesnegash33@gmail.com>
2026-04-23 13:36:13 +0300

    implementation of file imports

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
+from stats import number_of_words
+
+
 def get_book_text(filepath):
     with open(filepath, "r") as f:
         return f.read()
-
-def number_of_words(text):
-    return f"Found {len(text.split())} total words"
 
 def main():
     text = get_book_text("books/frankenstein.txt")
---
+++ stats.py
@@ -0,0 +1,2 @@
+def number_of_words(text):
+    return f"Found {len(text.split())} total words"
