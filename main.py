700fbe7216f313235af4e37f0d2974bb9f1751c8
Yohannes33 <yohannesnegash33@gmail.com>
2026-04-23 13:20:22 +0300

    Implement book text retrieval and display in main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("greetings boots")
+def get_book_text(filepath):
+    with open(filepath, "r") as f:
+        return f.read()
+
+
+def main():
+    text = get_book_text("books/frankenstein.txt")
+    print(text)
+
+
+main()
+
